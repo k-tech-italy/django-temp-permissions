162657c38d99b7f1f5ab57b6b816d110be192062
Gabriela Stepien <g.stepien@k-tech.it>
2025-09-17 14:55:40 +0100

    fix condition in test action
    fix ruff, update test action

--- src/django_temporary_permissions/apps.py
+++ src/django_temporary_permissions/apps.py
@@ -1,10 +1,10 @@
 """django-temporary-permissions app config."""
 
 from django.apps import AppConfig
-from typing_extensions import override
+from typing import override
 
 
-class Config(AppConfig):  # type: ignore  # noqa D101
+class Config(AppConfig):  # noqa D101
     verbose_name = "django-temporary-permissions"
     name = "django_temporary_permissions"
 
