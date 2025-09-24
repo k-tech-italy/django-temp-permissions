0ff5910bebb90997f82d4a090f4475266cc7affb
Gabriela Stepien <g.stepien@k-tech.it>
2025-09-24 13:59:55 +0100

    chore: ruff lint

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,6 +1,7 @@
 import pytest
 from django.contrib.auth import get_user_model
 
+
 def test_example():
     assert True
 
@@ -9,5 +10,5 @@ def test_example():
 def test_with_db():
     User = get_user_model()  # noqa: N806
 
-    User.objects.create_user(username='test', password='<PASSWORD>')
+    User.objects.create_user(username="test", password="<PASSWORD>")
     assert User.objects.count() == 1
