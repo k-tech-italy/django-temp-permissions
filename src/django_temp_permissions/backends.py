fcb07c4c43cb2cedb0f90e674a4a20f748429665
GigiusB <g.bronzini@gmail.com>
2025-11-07 13:43:43 +0000

    fix(Inactive-User): Fix: Inactive user was wrongly given the permission

--- src/django_temp_permissions/backends.py
+++ src/django_temp_permissions/backends.py
@@ -13,6 +13,9 @@ class TemporaryPermissionBackend(BaseBackend):
 
     def get_user_permissions(self, user_obj: User, obj: str | None = None) -> set:
         """Append the temporary permissions to the list of permissions."""
+        if not user_obj.is_active or user_obj.is_anonymous or obj is not None:
+            return set()
+
         permissions = TemporaryPermission.objects.get_active_permissions(user_obj)
         perms = permissions.values_list("content_type__app_label", "codename").order_by()
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,20 @@
 from datetime import timedelta
 
 import pytest
+from django.contrib.auth.models import Permission
 from django.utils import timezone
 
-from test_utils.factories import TemporaryPermissionFactory, UserFactory
+from test_utils.factories import UserFactory, TemporaryPermissionFactory
 
 
 @pytest.fixture
 def staff_user():
     return UserFactory(is_staff=True)
+
+
+@pytest.fixture
+def inactive_user():
+    return UserFactory(is_active=False)
 
 
 @pytest.fixture
@@ -27,3 +33,10 @@ def future_temporary_permission():
     return TemporaryPermissionFactory(
         start_datetime=base_time + timedelta(days=1), end_datetime=base_time + timedelta(days=2)
     )
+
+
+@pytest.fixture
+def change_user_perm():
+    ret = Permission.objects.get(codename="change_user")
+    ret._full_name = ret.content_type.app_label + "." + ret.codename
+    return ret
